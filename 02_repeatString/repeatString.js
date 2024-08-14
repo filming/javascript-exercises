677914bfe6daba455a2b6fa3865c03f33fb9f8b0
Filming <79931565+filming@users.noreply.github.com>
2024-08-14 19:19:14 -0400

    feat: Repeat a string x amount of times
    Refs: javascript-exercises/02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(str, numberOfRepeats) {
+    if (numberOfRepeats < 0){
+        return "ERROR";
+    }
 
+    let repeatedString = "";
+
+    for (let i = 0; i < numberOfRepeats; i++){
+        for (let j = 0; j < str.length; j++){
+            repeatedString += str[j];
+        }
+    }
+
+    return repeatedString;
 };
 
 // Do not edit below this line
