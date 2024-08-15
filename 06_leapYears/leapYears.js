4086e24aaf37a4162a4df7b7c580d8c967fa5888
Filming <79931565+filming@users.noreply.github.com>
2024-08-15 04:12:32 -0400

    feat: Determine if a year is a leap year
    Refs: javascript-exercises/06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let validLeapYear = false;
 
+    if (year % 100 === 0){
+        if (year % 400 === 0){
+            validLeapYear = true;
+        }
+    }
+    else if (year % 4 === 0){
+        validLeapYear = true;
+    }
+
+    return validLeapYear;
 };
 
 // Do not edit below this line
