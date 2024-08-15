ff4c6313bd7b41ad803fe0c1f2c5b12c07b0ed9f
Filming <79931565+filming@users.noreply.github.com>
2024-08-14 20:12:01 -0400

    feat: Reverse a string
    Refs: javascript-exercises/03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    const reversedStrChars = [];
 
+    for (let i = str.length-1; i >= 0; i--){
+        reversedStrChars.push(str[i]);
+    }
+
+    return reversedStrChars.join("");
 };
 
 // Do not edit below this line
