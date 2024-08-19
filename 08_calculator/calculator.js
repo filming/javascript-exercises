abc1e5e293471d2f3cb80c0816119e0d5637287d
Filming <79931565+filming@users.noreply.github.com>
2024-08-19 12:01:55 -0400

    feat: Complete calculator functions
    Refs: javascript-exercises/08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, currNum) => total += currNum, 0);
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  if (arr.length === 0){
+    return 0;
+  }
 
+  return arr.reduce((product, currNum) => product *= currNum, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let factorialVal = 1;
+
+  for (let i = num; i > 0; i--){
+    factorialVal *= i;
+  }
+
+  return factorialVal;
 };
 
 // Do not edit below this line
