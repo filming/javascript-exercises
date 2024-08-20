25666d15045bd0d1976bb2ae1d4601bc357a4d77
Filming <79931565+filming@users.noreply.github.com>
2024-08-19 20:29:24 -0400

    Complete getTheTitles function
    Refs: javascript-exercises/11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    return books.reduce((bookTitles, currBook) => {
+        bookTitles.push(currBook.title);
+        return bookTitles;
+    }, []);
 };
 
 // Do not edit below this line
