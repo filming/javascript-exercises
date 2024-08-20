cc79263b47a73087c07be8a544ee814dfc87db54
Filming <79931565+filming@users.noreply.github.com>
2024-08-19 20:18:16 -0400

    feat: Complete fibonacci function
    Refs: javascript-exercises/10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    // handling edge cases
+    if (num < 0){
+        return "OOPS";
+    }
+    else if (num < 2){
+        return +num; // '+' makes sure that strings get converted into numbers on return
+    }
 
+    // create fibonacci sequence array
+    const fibonacciArr = [0, 1];
+
+    while (fibonacciArr.length <= num){
+        const sumOfPreviousTwo = fibonacciArr[fibonacciArr.length - 1] + fibonacciArr[fibonacciArr.length - 2];
+        fibonacciArr.push(sumOfPreviousTwo);
+    }
+
+    return fibonacciArr[fibonacciArr.length-1];
 };
 
 // Do not edit below this line
