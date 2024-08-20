02efe3476b880fb79e99eb2209dde62f363a3f84
Filming <79931565+filming@users.noreply.github.com>
2024-08-19 20:54:06 -0400

    Complete findTheOldest function
    Refs: javascript-exercises/12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    
+    return people.reduce((oldestPerson, currPerson) => {
+
+        const currPersonYearOfDeath = "yearOfDeath" in currPerson ? currPerson.yearOfDeath : new Date().getFullYear();
+        const oldestPersonYearOfDeath = "yearOfDeath" in oldestPerson ? oldestPerson.yearOfDeath : new Date().getFullYear();
+
+        if ((currPersonYearOfDeath - currPerson.yearOfBirth) > (oldestPersonYearOfDeath - oldestPerson.yearOfBirth)){
+            oldestPerson = currPerson;
+        }
+
+        return oldestPerson;
+    });
+    
 
 };
 
